ef83a82f72da722870f6bd8907119b3748e25ad2
Artsem Harustovich <artsem_harustovich@epam.com>
2023-08-18 18:57:54 +0200

    Added pre- and post-conditions, screenshots of results

--- src/test/java/TimeoutTest.java
+++ src/test/java/TimeoutTest.java
@@ -1,3 +1,6 @@
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.time.Duration;
@@ -5,10 +8,25 @@ import java.time.Duration;
 import static org.junit.jupiter.api.Assertions.assertTimeout;
 
 public class TimeoutTest {
-    Timeout timeout = new Timeout();
+    static Timeout timeout;
+
+    @BeforeAll
+    public static void prepTestBeforeAll() {
+        System.out.println("Timeout tests are started!");
+    }
+
+    @BeforeEach
+    public void prepTestBeforeEach() {
+        timeout = new Timeout();
+    }
 
     @Test
-    public void timeoutTest() {
+    public void testTimeout() {
         assertTimeout(Duration.ofSeconds(10), () -> timeout.sleep(5));
+    }
+
+    @AfterAll
+    public static void testAfterAll() {
+        System.out.println("Timeout tests are finished!");
     }
 }
